6e8362b9997e8b6dfc987bb5b7c178d1434d86dc
SuperJan92 <janvanerkel92@gmail.com>
2024-07-29 21:13:59 +0200

    Functie taxonomy added

--- functions.php
+++ functions.php
@@ -12,25 +12,52 @@ require_once __DIR__ . '/src/StarterSite.php';
 Timber\Timber::init();
 
 // Sets the directories (inside your theme) to find .twig files.
-Timber::$dirname = [ 'templates', 'views' ];
+Timber::$dirname = ['templates', 'views'];
 
 new StarterSite();
 
-add_action( 'init', 'create_post_type' );
-function create_post_type() {
-    register_post_type( 'medewerker',
-        array(
-            'labels' => array(
-                'name' => __( 'Medewerker' ),
-                'singular_name' => __( 'Medewerkers' ),
-                'add_new' => __( 'Medewerker toevoegen' )
-            ),
-            'public' => true,
-            'has_archive' => true,
-            'supports' => array('title', 'editor', 'thumbnail', 'revisions'),
-            'taxonomies' => array('category', 'post_tag'),
-            'menu_icon' => 'dashicons-groups'
-        )
-    );
-}
+//Medewerkers post type
+add_action('init', function () {
+    register_post_type('medewerker', [
+        'label' => __('Medewerkers', 'txtdomain'),
+        'public' => true,
+        'menu_position' => 5,
+        'menu_icon' => 'dashicons-businessman',
+        'supports' => ['title', 'editor', 'thumbnail', 'author', 'revisions', 'comments'],
+        'show_in_rest' => true,
+        'rewrite' => ['slug' => 'medewerker'],
+        'taxonomies' => ['functie'],
+        'labels' => [
+            'singular_name' => __('Book', 'txtdomain'),
+            'add_new_item' => __('Add new book', 'txtdomain'),
+            'new_item' => __('New book', 'txtdomain'),
+            'view_item' => __('View book', 'txtdomain'),
+            'not_found' => __('No books found', 'txtdomain'),
+            'not_found_in_trash' => __('No books found in trash', 'txtdomain'),
+            'all_items' => __('All books', 'txtdomain'),
+            'insert_into_item' => __('Insert into book', 'txtdomain')
+        ],
+    ]);
 
+    register_taxonomy('functie', ['medewerker'], [
+        'label' => __('Functie', 'txtdomain'),
+        'hierarchical' => true,
+        'rewrite' => ['slug' => 'medewerker-functie'],
+        'show_admin_column' => true,
+        'show_in_rest' => true,
+        'labels' => [
+            'singular_name' => __('Functie', 'txtdomain'),
+            'all_items' => __('Alle functies', 'txtdomain'),
+            'edit_item' => __('Functie aanpassen', 'txtdomain'),
+            'view_item' => __('Functie bekijken', 'txtdomain'),
+            'update_item' => __('Update functie', 'txtdomain'),
+            'add_new_item' => __('Functie toevoegen', 'txtdomain'),
+            'new_item_name' => __('Nieuwe functie naam', 'txtdomain'),
+            'search_items' => __('Functies zoeken', 'txtdomain'),
+            'parent_item' => __('Parent functie', 'txtdomain'),
+            'parent_item_colon' => __('Parent functie:', 'txtdomain'),
+            'not_found' => __('Geen functies gevonden', 'txtdomain'),
+        ]
+    ]);
+    register_taxonomy_for_object_type('functie', 'medewerker');
+});
