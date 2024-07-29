e1db1127ecd593477bf50893a1a5751ad4fee5f2
SuperJan92 <janvanerkel92@gmail.com>
2024-07-29 21:21:41 +0200

    CPT labels and taxonomy labels changed

--- functions.php
+++ functions.php
@@ -19,7 +19,7 @@ new StarterSite();
 //Medewerkers post type
 add_action('init', function () {
     register_post_type('medewerker', [
-        'label' => __('Medewerkers', 'txtdomain'),
+        'label' => __('Medewerkers'),
         'public' => true,
         'menu_position' => 5,
         'menu_icon' => 'dashicons-businessman',
@@ -28,35 +28,36 @@ add_action('init', function () {
         'rewrite' => ['slug' => 'medewerker'],
         'taxonomies' => ['functie'],
         'labels' => [
-            'singular_name' => __('Book', 'txtdomain'),
-            'add_new_item' => __('Add new book', 'txtdomain'),
-            'new_item' => __('New book', 'txtdomain'),
-            'view_item' => __('View book', 'txtdomain'),
-            'not_found' => __('No books found', 'txtdomain'),
-            'not_found_in_trash' => __('No books found in trash', 'txtdomain'),
-            'all_items' => __('All books', 'txtdomain'),
-            'insert_into_item' => __('Insert into book', 'txtdomain')
+            'name' => __( 'Medewerkers' ),
+            'singular_name' => __( 'Medewerkers' ),
+            'add_new_item' => __( 'Medewerker toevoegen' ),
+            'add_new' => __( 'Medewerker toevoegen' ),
+            'edit_item' => __( 'Medewerker aanpassen' ),
+            'featured_image' => __( 'Foto' ),
+            'set_featured_image' => __( 'Upload foto' ),
+            'remove_featured_image' => __( 'Foto verwijderen' ),
+            'menu_name' => __( 'Medewerkers' ),
         ],
     ]);
 
     register_taxonomy('functie', ['medewerker'], [
-        'label' => __('Functie', 'txtdomain'),
+        'label' => __('Functie'),
         'hierarchical' => true,
         'rewrite' => ['slug' => 'medewerker-functie'],
         'show_admin_column' => true,
         'show_in_rest' => true,
         'labels' => [
-            'singular_name' => __('Functie', 'txtdomain'),
-            'all_items' => __('Alle functies', 'txtdomain'),
-            'edit_item' => __('Functie aanpassen', 'txtdomain'),
-            'view_item' => __('Functie bekijken', 'txtdomain'),
-            'update_item' => __('Update functie', 'txtdomain'),
-            'add_new_item' => __('Functie toevoegen', 'txtdomain'),
-            'new_item_name' => __('Nieuwe functie naam', 'txtdomain'),
-            'search_items' => __('Functies zoeken', 'txtdomain'),
-            'parent_item' => __('Parent functie', 'txtdomain'),
-            'parent_item_colon' => __('Parent functie:', 'txtdomain'),
-            'not_found' => __('Geen functies gevonden', 'txtdomain'),
+            'singular_name' => __('Functie'),
+            'all_items' => __('Alle functies'),
+            'edit_item' => __('Functie aanpassen'),
+            'view_item' => __('Functie bekijken'),
+            'update_item' => __('Update functie'),
+            'add_new_item' => __('Functie toevoegen'),
+            'new_item_name' => __('Nieuwe functie naam'),
+            'search_items' => __('Functies zoeken'),
+            'parent_item' => __('Parent functie'),
+            'parent_item_colon' => __('Parent functie:'),
+            'not_found' => __('Geen functies gevonden'),
         ]
     ]);
     register_taxonomy_for_object_type('functie', 'medewerker');
